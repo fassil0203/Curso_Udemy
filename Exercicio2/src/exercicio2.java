036ba1664eff56cf8e279a02ed845b8f3af7546b
fassil0203 <72141864+fassil0203@users.noreply.github.com>
2022-03-31 17:44:15 -0300

    Add files via upload

---
+++ Exercicio2/src/exercicio2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class exercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N=sc.nextInt();
+		
+		if(N % 2 ==0) {
+			System.out.println(" O Número é Par ");
+					
+		}
+		else {
+			System.out.println( " O Número é Impar ");
+		}
+			sc.close();
+				
+
+	}
+
+}
